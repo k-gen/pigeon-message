0e02973f41c14deae360c7544cc3d71c14a2dfe0
Yuki Kagen <yuki.kagen@tam-bourine.co.jp>
2020-11-14 23:09:25 +0900

    モーダルにフォームパーツを追加

--- app.js
+++ app.js
@@ -31,12 +31,56 @@ app.event('app_home_opened', async ({ context, event, say }) => {
     }
 });
 
-const modal = (props = {}) => jsxslack`
+const options = (count, start, suffix) => {
+    return [...Array(count)].map((_, i) => {
+        const s = (i + start).toString();
+        return jsxslack`
+            <Option value="${s}">${s.padStart(2, '0')}${suffix}</Option>
+        `
+    });
+};
+
+const TimePicker = props => jsxslack`
+    <Section>
+        <b>${props.label}</b>
+    </Section>
+    <Actions id="${props.id}">
+        <Select name="hour" value="${props.hour}" placeholder="時">
+            <Optgroup label="午前">${options(12, 0, '時')}</Optgroup>
+            <Optgroup label="午後">${options(12, 12, '時')}</Optgroup>
+        </Select>
+        <Select name="minute" value="${props.minute}" placeholder="分">
+            ${options(60, 0, '分')}
+        </Select>
+    </Actions>
+
+    <!-- error message -->
+    ${props.error & jsxslack`<Context>:warning: <b>${props.error}</b></Context>`}
+
+    <Input type="hidden" name="hour" value="${props.hour}" />
+    <Input type="hidden" name="minute" value="${props.minute}" />
+`
+
+const modal = props => jsxslack`
     <Modal title="伝言を送る" callbackId="post">
         <Section>
             私にお任せ下さい！
             <Image src="https://source.unsplash.com/ic-13C3QhAI/256x256" alt="鳩" />
         </Section>
+
+        <Textarea id="message" name="message" label="伝言" placeholder="伝言をどうぞ…" required />
+        <UsersSelect id="users" name="users" label="送付先" multiple required />
+        <DatePicker id="date" name="date" label="日付" required />
+
+        <${TimePicker}
+            id="time"
+            label="時刻"
+            hour="${props.hour}"
+            minute="${props.minute}"
+            error="${props.timePickerError}"
+        />
+
+        <Input type="hidden" name="userId" value="${props.userId}" />
     </Modal>
 `
 
@@ -46,7 +90,7 @@ app.action('post', ({ ack, body, context }) => {
     app.client.views.open({
         token: context.botToken,
         trigger_id: body.trigger_id,
-        view: modal(),
+        view: modal({ userId: body.user.id }),
     });
 });
 
